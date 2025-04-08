ad4c4a59c3bb7b45d5e7cbb4c3e6afb2f9a61d90
eliezerraj <eliezerraj@gmail.com>
2025-04-08 12:22:18 -0300

    feat(api):refactor

--- api/api.go
+++ api/api.go
@@ -31,6 +31,13 @@ func NewRestApiService() *ApiService{
 	}
 }
 
+type HttpClient struct{
+	Url 	string
+	Method 	string
+	Timeout time.Duration
+	Headers *map[string]string
+}
+
 func (a *ApiService) CallApi(ctx context.Context, 
 							url string,
 							method string,
@@ -71,6 +78,69 @@ func (a *ApiService) CallApi(ctx context.Context,
 	}
 	if (header_x_resquest_id != nil){
 		req.Header.Add("X-Request-Id", *header_x_resquest_id)
+	}
+
+	resp, err := client.Do(req.WithContext(ctx))
+	if err != nil {
+		childLogger.Error().Err(err).Send()
+		return nil, http.StatusServiceUnavailable, errors.New(err.Error())
+	}
+
+	childLogger.Debug().Int("StatusCode :", resp.StatusCode).Msg("")
+	switch (resp.StatusCode) {
+		case 401:
+			return nil, http.StatusUnauthorized, ErrUnauthorized
+		case 403:
+			return nil, http.StatusForbidden, ErrHTTPForbiden
+		case 200:
+		case 400:
+			return nil, http.StatusNotFound, ErrNotFound
+		case 404:
+			return nil, http.StatusNotFound, ErrNotFound
+		default:
+			return nil, http.StatusInternalServerError, ErrServer
+	}
+
+	result := body
+	err = json.NewDecoder(resp.Body).Decode(&result)
+    if err != nil {
+		childLogger.Error().Err(err).Send()
+		return nil, http.StatusInternalServerError, errors.New(err.Error())
+    }
+
+	return result, http.StatusOK ,nil
+}
+
+// Above call rest api 
+func (a *ApiService) CallRestApi(ctx context.Context, 
+							httpClient HttpClient,
+							body interface{}) (interface{}, int, error){
+	childLogger.Print("...................................")
+	childLogger.Debug().Str("func","CallRestApi").
+						Interface("httpClient", httpClient).
+						Interface("body", body).Send()
+	childLogger.Print("...................................")
+
+	transportHttp := &http.Transport{}
+	
+	client := http.Client{
+		Transport: otelhttp.NewTransport(transportHttp),
+		Timeout: time.Second * httpClient.Timeout,
+	}
+
+	payload := new(bytes.Buffer) 
+	if body != nil{
+		json.NewEncoder(payload).Encode(body)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, httpClient.Method, httpClient.Url, payload)
+	if err != nil {
+		childLogger.Error().Err(err).Send()
+		return nil, http.StatusBadGateway, errors.New(err.Error())
+	}
+
+	for key, value := range *httpClient.Headers {
+		req.Header.Set(key, value)
 	}
 
 	resp, err := client.Do(req.WithContext(ctx))
