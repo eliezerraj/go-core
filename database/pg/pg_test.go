efb8cadd772dd83609815f3f5432665be2ad5122
eliezerraj <eliezerraj@gmail.com>
2025-05-03 15:59:27 -0300

    feat(coreJson):add trace-id

--- database/pg/pg_test.go
+++ database/pg/pg_test.go
@@ -3,8 +3,20 @@ package pg
 import (
 	"testing"
 	"context"
+	"encoding/json"
 	"time"
 )
+
+type PoolStats struct {
+	AcquireCount        int64 `json:"acquire_count"`
+	AcquiredConns       int32 `json:"acquired_conns"`
+	CanceledAcquireCount int64 `json:"canceled_acquire_count"`
+	ConstructingConns   int32 `json:"constructing_conns"`
+	EmptyAcquireCount   int64 `json:"empty_acquire_count"`
+	IdleConns           int32 `json:"idle_conns"`
+	MaxConns            int32 `json:"max_conns"`
+	TotalConns          int32 `json:"total_conns"`
+}
 
 func TestGoCore_DatabasePGServer(t *testing.T){
 	var databasePGServer DatabasePGServer
@@ -36,4 +48,22 @@ func TestGoCore_DatabasePGServer(t *testing.T){
 	databasePG.ReleaseTx(conn)
 	
 	t.Logf("databasePG: %v", databasePG)
+
+	stats := databasePG.Stat()
+
+	statsJSON := PoolStats{
+		AcquireCount:         stats.AcquireCount(),
+		AcquiredConns:        stats.AcquiredConns(),
+		CanceledAcquireCount: stats.CanceledAcquireCount(),
+		ConstructingConns:    stats.ConstructingConns(),
+		EmptyAcquireCount:    stats.EmptyAcquireCount(),
+		IdleConns:            stats.IdleConns(),
+		MaxConns:             stats.MaxConns(),
+		TotalConns:           stats.TotalConns(),
+	}
+
+	// Convert to JSON
+	jsonBytes, _ := json.MarshalIndent(statsJSON, "", "  ")
+
+	t.Logf("stats: %v", string(jsonBytes) )
 }
