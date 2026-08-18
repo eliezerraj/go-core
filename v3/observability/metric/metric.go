7ac4539220020bd88b29f8b0fb33f5a972c07c83
eliezerraj <eliezerraj@gmail.com>
2026-08-18 00:39:00 -0300

    feat:otel

---
+++ v3/observability/metric/metric.go
@@ -0,0 +1,52 @@
+package metric
+
+import(
+	"context"
+
+	"go.opentelemetry.io/otel/attribute"
+
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/exporters/prometheus"
+	
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
+)
+
+type InfoMetric struct {
+	Name			string `json:"service_name,omitempty"`
+	Version			string `json:"service_version,omitempty"`
+	ServiceType		string `json:"service_type,omitempty"`
+	Env				string `json:"enviroment,omitempty"`
+	Account			string `json:"account,omitempty"`
+}
+
+// About initialize MeterProvider with Prometheus exporter
+func NewMeterProvider(	ctx context.Context, 
+						infoMetric InfoMetric) (*sdkmetric.MeterProvider, error) {
+
+	// 1. Configurar o Recurso OTel
+	res, err := resource.New(ctx,
+							resource.WithSchemaURL(semconv.SchemaURL),
+							resource.WithAttributes(
+								semconv.ServiceNameKey.String(infoMetric.Name),
+								attribute.String("version", infoMetric.Version),
+							),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// 2. Criar o Prometheus Exporter
+	exporter, err := prometheus.New()
+	if err != nil {
+		return nil, err
+	}
+
+	// 3. Criar o MeterProvider, usando o Prometheus Exporter como Reader.
+	meterProvider := sdkmetric.NewMeterProvider(
+		sdkmetric.WithResource(res),
+		sdkmetric.WithReader(exporter),
+	)
+
+	return meterProvider, nil
+}
