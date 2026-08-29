7ee29b3d689fdd539ba0091703a32001fee691d3
eliezerraj <eliezerraj@gmail.com>
2026-08-28 22:05:25 -0300

    fix:kafka consumer

--- v3/event/kafka/consumer/consumer.go
+++ v3/event/kafka/consumer/consumer.go
@@ -9,7 +9,7 @@ import(
 )
 
 type ConsumerWorker struct{
-	consumer        	*kafka.Consumer
+	Consumer        	*kafka.Consumer
 }
 
 func NewConsumerWorker(kafkaConfig *kafka.ConfigMap) (*ConsumerWorker, error) {
@@ -21,14 +21,14 @@ func NewConsumerWorker(kafkaConfig *kafka.ConfigMap) (*ConsumerWorker, error) {
 		return nil, err
 	}
 	return &ConsumerWorker{
-		consumer: consumer,
+		Consumer: consumer,
 	}, nil
 }
 
 func (c *ConsumerWorker) SubscribeTopics(topics []string) error {
 	logger.InfoOutCtx("subscribing to kafka topics")
 	
-	err := c.consumer.SubscribeTopics(topics, nil)
+	err := c.Consumer.SubscribeTopics(topics, nil)
 	if err != nil {
 		logger.ErrorOutCtx("failed to subscribe to kafka topics", zap.Error(err))
 		return err
@@ -39,7 +39,7 @@ func (c *ConsumerWorker) SubscribeTopics(topics []string) error {
 // Close closes the consumer and releases any resources associated with it.
 func (c *ConsumerWorker) Close() error {
 	logger.InfoOutCtx("closing kafka consumer")
-	err := c.consumer.Close()
+	err := c.Consumer.Close()
 	if err != nil {
 		logger.ErrorOutCtx("failed to close kafka consumer", zap.Error(err))
 		return err
@@ -50,7 +50,7 @@ func (c *ConsumerWorker) Close() error {
 // CommitMessage commits the offset of the provided message to Kafka.
 func (c *ConsumerWorker) CommitMessage(msg *kafka.Message) error {
 	logger.InfoOutCtx("committing message offset to kafka")
-	_, err := c.consumer.CommitMessage(msg)
+	_, err := c.Consumer.CommitMessage(msg)
 	if err != nil {
 		logger.ErrorOutCtx("failed to commit message offset", zap.Error(err))
 		return err
